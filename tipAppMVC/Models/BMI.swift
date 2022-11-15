cb3ff38b35328f0876787fab840428060fae549e
k0ndrat <88559542+k0ndrat@users.noreply.github.com>
2022-11-15 23:44:47 +0200

    2

---
+++ tipAppMVC/Models/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  tipAppMVC
+//
+//  Created by Dima Kondratiev on 15.11.2022.
+//
+
+
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
